8113555867991223d8be0cba3e7ec4a0f3d47432
cdw200806 <924673193@qq.com>
2025-04-17 21:08:51 +0800

    commit...

--- src/main/java/com/example/T8.java
+++ src/main/java/com/example/T8.java
@@ -3,24 +3,26 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.*;
 
-public class T7 extends Thread {
-
-    volatile boolean b = true;
+public class T8 extends Thread {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(1);
         ConcurrentHashMap map = new ConcurrentHashMap(4);
-        Object o = new Object();
-        map.put(null, 1);
-
-
-        Class<List> listClass = List.class;
-
-        List<String> l = new ArrayList<>();
-        System.out.println(1);
+        map.put(1, 1);
+        Callable callable = () -> {
+            System.out.println(11111);
+            while (true) {
+                map.compute(1, (k, v) -> v);
+            }
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        FutureTask futureTask = new FutureTask<>(callable);
+        executorService.submit(futureTask);
+        System.out.println(22222);
+        while (true) {
+            map.put(1, 1);
+        }
     }
 
     @Override
