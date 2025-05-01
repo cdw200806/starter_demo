7678f36d70b42cf7f05f901807c8dac8cf61b442
cdw200806 <924673193@qq.com>
2025-05-01 22:14:52 +0800

    commit...

---
+++ src/main/java/com/example/I2.java
@@ -0,0 +1,6 @@
+package com.example;
+
+
+//接口竟然也能继承接口。
+public interface I2 extends TestExtendsInterFace {
+}
--- src/main/java/com/example/T7.java
+++ src/main/java/com/example/T7.java
@@ -1,26 +1,62 @@
 package com.example;
 
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class T7 extends Thread {
 
     volatile boolean b = true;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
+
+        ObjectInputStream o = new ObjectInputStream(new FileInputStream("a.txt"));
+        Object conMap = o.readObject();
+
+        long tt = Long.MAX_VALUE;
+        System.out.println(tt + 1 == Long.MIN_VALUE);
         System.out.println(1);
-        ConcurrentHashMap map = new ConcurrentHashMap(4);
+        ConcurrentHashMap map = new ConcurrentHashMap(16);
         Object o = new Object();
-        map.put(null, 1);
+
+        map.put(map, map);
+
+        ConcurrentHashMap map1 = new ConcurrentHashMap();
+        map1.put(map1, map1);
+
+        System.out.println(map.equals(map1));
+        System.out.println(map.hashCode());
+
+
+        map.put(1, 1);
+        //map.replace();
+
+
+        for (int i = 0; i < 10; i++) {
+            map.put(i * 32 + 1, 1);
+
+        }
+        map.put(1, 1);
+
+        map.get(1);
+
+        map.compute(1, (k, v) -> v);
+
+        map.entrySet();
 
 
         Class<List> listClass = List.class;
 
         List<String> l = new ArrayList<>();
         System.out.println(1);
+
+        List<String> ll = new LinkedList<>();
+
+
+        List<? super ArrayList<String>> t = new ArrayList<List<String>>();
     }
 
     @Override
